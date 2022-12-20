1f75bfbc04d8b37b24fe97c2785e40040ea14bd5
Dcs <81396710+Dcruzs@users.noreply.github.com>
2022-12-20 17:41:13 -0500

    _Added datasets description in the sidePanel

--- global.R
+++ global.R
@@ -2,15 +2,15 @@ library(shiny)
 library(DT)
 library(vegan)
 
-setwd("D:/DataQuest/ShinyApp/Spdiv/SpDiv_ApProject")
+setwd("D:/..../Spdiv/SpDiv_ApProject")
 
 #datasets
 
-dune <- read.csv("D:/DataQuest/ShinyApp/Spdiv/SpDiv_ApProject/data/dune.csv")
+dune <- read.csv("D:/....//Spdiv/SpDiv_ApProject/data/dune.csv")
 head(dune)
 
-vegan.BCI <- read.csv("D:/DataQuest/ShinyApp/Spdiv/SpDiv_ApProject/data/vegan.BCI.csv")
+vegan.BCI <- read.csv("D:/..../Spdiv/SpDiv_ApProject/data/vegan.BCI.csv")
 head(vegan.BCI)
 
-mite.data <- read.csv("D:/DataQuest/ShinyApp/Spdiv/SpDiv_ApProject/data/mite.csv")
+mite.data <- read.csv("D:/....//Spdiv/SpDiv_ApProject/data/mite.csv")
 head(mite.data)
