50b5182a1714f10d422a89b168eac81978b905ca
Dcs <81396710+Dcruzs@users.noreply.github.com>
2022-12-20 17:32:19 -0500

    _Added descriptions of the datatasets into the side Panel

--- global.R
+++ global.R
@@ -1,17 +1,16 @@
 library(shiny)
+library(DT)
+library(vegan)
 
-setwd("D:/...")
+setwd("D:/DataQuest/ShinyApp/Spdiv/SpDiv_ApProject")
 
 #datasets
 
-dune <- read.csv("D:/Spdiv/SpDiv_ApProject/data/dune.csv")
+dune <- read.csv("D:/DataQuest/ShinyApp/Spdiv/SpDiv_ApProject/data/dune.csv")
 head(dune)
 
-dune_moistureLevel <- read.csv("D:/Spdiv/SpDiv_ApProject/data/dune_moistureLevel.csv")
-head(dune_moistureLevel)
-
-flutter <- read.csv("D:/Spdiv/SpDiv_ApProject/data/flutter.csv")
-head(flutter)
-
-vegan.BCI <- read.csv("D:/Spdiv/SpDiv_ApProject/data/vegan.BCI.csv")
+vegan.BCI <- read.csv("D:/DataQuest/ShinyApp/Spdiv/SpDiv_ApProject/data/vegan.BCI.csv")
 head(vegan.BCI)
+
+mite.data <- read.csv("D:/DataQuest/ShinyApp/Spdiv/SpDiv_ApProject/data/mite.csv")
+head(mite.data)
--- pickdata_app.R
+++ pickdata_app.R
@@ -1,29 +1,23 @@
 library(shiny)
 library(DT)
 
-#### This code applies DT () to customize tables of the dataset chosen #########
-
-# Define UI for application that draws a histogram
 ui <- fluidPage(
   
-  # Application title
+  
   titlePanel("Downloading Dataset of Species Diversity"),
   
-  # Sidebar with a slider input for number of bins 
+  
   sidebarLayout(
     sidebarPanel(
       selectInput("dataset", "Pick a dataset:", 
-                                    c("dune", "dune_moistureLevel", "flutter", "vegan.BCI"))
-      
+                  c("dune", "mite.data", "vegan.BCI")),
+      textOutput("description") ### add description of the dataset selected into the side panel instead of the mainPanel
       
     ), 
-    # Button to download teh data (optional)
-    # downloadButton("downloadData", "Download")
     
-    
-    # Main panel for displaying outputs ----
     mainPanel(
-      DTOutput("table")
+      dataTableOutput("table")
+      
       
     )
   )
@@ -31,34 +25,31 @@ ui <- fluidPage(
 
 server <- function(input, output) {
   
-  # Reactive value for selected dataset ----
+  desc <- reactive({
+    switch(input$dataset, # need to edit the texts
+           "dune" = "Vegetation data of the Dutch Dune Meadows, Terschelling, Netherlands. The study sampled 20 sites of 2x 2 square meters each and found 30 species of plants. For more details, check out R - Vegan package (Oksanen et al. 2022).",
+           "mite.data" = "The data includes morpho-species of oribatid mites (also known as moss or beetle mites) found in the vicinity of Lac Geai (in Québec, Canada). Mites were collected from Sphagnum moss samples (5 cm in diameter and 7 cm deep). The table includes 70 samples (rows) and 35 mite species (columns). For more details, check out R - Vegan package (Oksanen et al. 2022).",
+           "vegan.BCI" = "On the Panamanian island of Barro Colorado (BCI), data from floristic surveys revealed the number of trees in a plot of 1-hectare . The table displays the 225 species found (columns) in 50 plots/samples (rows). For more details, check out R - Vegan package (Oksanen et al. 2022)."
+    ) 
+  })
+  output$description = renderText(desc())
+  
+  
   myCSV <- reactive({
     switch(input$dataset,
            "dune" = dune,
-           "dune_moistureLevel" = dune_moistureLevel,
-           "flutter" = flutter,
+           "mite.data" = mite.data,
            "vegan.BCI" = vegan.BCI)
   })
   
   
-  # Table of selected dataset ----
-      #https://rstudio.github.io/DT/
   output$table = renderDT(myCSV(), options = list(
     pageLength = 10)
   )
   
-  # Downloadable csv of selected dataset ----
-  #output$downloadData <- downloadHandler(
-  # filename = function() {
-  # paste(input$dataset, ".csv", sep = "")
-  #},
-  #content = function(file) {
-  # write.csv(datasetInput(), file, row.names = FALSE)
-  #}
-  #)
+  
   
 }
 
 
-# Run the application 
 shinyApp(ui = ui, server = server)
